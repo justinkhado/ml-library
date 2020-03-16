b698e620fec62551db9f17e7132e447314532965
justinkhado <justinkhado@gmail.com>
2020-03-16 03:28:40 -0700

    completed logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -14,16 +14,33 @@ class LogisticRegression(object):
         '''
         Fits the data and target values to the model
         '''
-        pass
+        # Transform X so that each column is a sample
+        X = X.T
+        num_features, num_samples = X.shape
+        
+        self.weights = np.zeros(num_features).reshape(num_features, 1)
+        self.bias = 0.0
+
+        for i in range(num_samples):
+            # affine function
+            Z = np.dot(self.weights.T, X) + self.bias
+            # sigmoid function
+            sigmoid = 1 / (1 + np.exp(-Z))
+
+            # partial derivative of log loss function with respect to weights
+            dL_dw = (1/num_samples) * np.sum(np.multiply((sigmoid - y), X), axis=1).reshape(num_features, 1)
+            # partial derivative of log loss function with respect to bias
+            dL_db = (1/num_samples) * np.sum(sigmoid - y)
+
+            # update weights and bias
+            self.weights -= self.learning_rate * dL_dw
+            self.bias -= self.learning_rate * dL_db
 
     def predict(self, X):
         '''
         Returns the predictions for a dataset
         '''
-        pass
-
-    @staticmethod
-    def binary_cross_entropy(y_true, y_pred):
-        '''
-        Returns the cost based on the binary cross entropy loss function
-        '''
+        X = X.T
+        Z = np.dot(self.weights.T, X) + self.bias
+        sigmoid = 1 / (1 + np.exp(-Z))
+        return (sigmoid >= 0.5).astype(int).reshape(-1)
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Normalizer
 from linear_regression import LinearRegression
 
+'''
 X, y = load_boston(return_X_y=True)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
@@ -18,3 +19,20 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 print(model.mean_squared_error(y_test, predictions) ** (1/2))
+'''
+
+from sklearn.datasets import load_breast_cancer
+from logistic_regression import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+X, y = load_breast_cancer(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print(confusion_matrix(y_test, predictions))
+print()
+print(classification_report(y_test, predictions))
